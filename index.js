965d4a8b817dcb58650808811463470ff9804e20
Adam Soltys <asoltys@gmail.com>
2022-09-23 21:28:52 +0000

    deploy script

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-import fastify from "fastify";
-import { exec } from "child_process";
+const { exec } = require("child_process");
+const crypto = require("crypto");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-let app = fastify();
+const { SECRET: secret } = process.env;
 
-let host = process.env.HOST || "0.0.0.0";
-let port = process.env.PORT || 3000;
+// For these headers, a sigHashAlg of sha1 must be used instead of sha256
+// GitHub: X-Hub-Signature
+// Gogs:   X-Gogs-Signature
+const sigHeaderName = "X-Hub-Signature-256";
+const sigHashAlg = "sha256";
 
-app.listen({ host, port }, (e, a) => {
-  e && err(e) && process.exit(1);
-  console.log(`Coinos CI server listening on ${a}`);
-});
+const app = express();
 
-app.post("/hook", async (req, res) => {
-  console.log(req.headers)
+// Saves a valid raw JSON body to req.rawBody
+// Credits to https://stackoverflow.com/a/35651853/90674
+app.use(
+  bodyParser.json({
+    verify: (req, res, buf, encoding) => {
+      if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || "utf8");
+      }
+    },
+  })
+);
+
+function verifyPostData(req, res, next) {
+  if (!req.rawBody) {
+    return next("Request body empty");
+  }
+
+  const sig = Buffer.from(req.get(sigHeaderName) || "", "utf8");
+  const hmac = crypto.createHmac(sigHashAlg, secret);
+  const digest = Buffer.from(
+    sigHashAlg + "=" + hmac.update(req.rawBody).digest("hex"),
+    "utf8"
+  );
+  if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+    return next(
+      `Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`
+    );
+  }
+
+  return next();
+}
+
+app.post("/hook", verifyPostData, function (req, res) {
   exec("sh deploy", (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
@@ -20,6 +53,12 @@ app.post("/hook", async (req, res) => {
       console.log(`exec error: ${error}`);
     }
   });
-
-  res.send({});
+  res.status(200).send("Request body was signed");
 });
+
+app.use((err, req, res, next) => {
+  if (err) console.error(err);
+  res.status(403).send("Request body was not signed or verification failed");
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
